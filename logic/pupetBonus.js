90a895d6b7df8013855a9c2e3b66d5877096fba5
Baruch-Karlin <bukbeak@gmail.com>
2021-05-31 10:11:33 +0300

    logic for bonus done

--- logic/pupetBonus.js
+++ logic/pupetBonus.js
@@ -13,9 +13,22 @@ const puppeteer = require('puppeteer');
 
     const data = await page.evaluate(() => {
         const body = document.querySelector("tbody");
-        const trs = body.getElementsByTagName("tr");  
+        const trs = body.getElementsByTagName("tr");
         const arr = Array.prototype.slice.call(trs);
-       
+        const tds = arr.map(e => {
+            return (e.cells[1])
+        })
+        // tds.forEach(e => console.log(Array.prototype.slice.call(e.getElementsByTagName('a'))))
+        const list = tds.map(e => Array.prototype.slice.call(e.getElementsByTagName('a')))
+        const as = a = list.map(e => (e[0]))      
+        const filter = as.filter(e => {
+            if (e !== undefined) {
+                return(e)
+            }
+        })
+        filter.forEach(e => console.log(e.href))
+
+
 
         // const href = trs[5].getElementsByTagName("td")[1].firstElementChild.href 
         return href;
